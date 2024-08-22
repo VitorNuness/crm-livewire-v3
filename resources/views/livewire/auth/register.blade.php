30d2e5f65b7847ccddafaf6ced8954293918a583
VitorNuness <vitor1nuness@gmail.com>
2024-08-22 12:09:05 -0300

    crm-2: adding link to login and form title

--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -1,4 +1,4 @@
-<x-card shadow class="mx-auto w-[350px]">
+<x-card title="Register" shadow class="mx-auto w-[450px]">
     <x-form wire:submit="submit">
         <x-input label="Name" wire:model="name" />
         <x-input label="Email" wire:model="email" />
@@ -6,6 +6,7 @@
         <x-input label="Password" wire:model="password" type="password" />
     
         <x-slot:actions>
+            <x-button label="I has an account" :link="route('login')" class="btn-ghost" />
             <x-button label="Cancel" type="reset" />
             <x-button label="Register" class="btn-primary" type="submit" spinner="submit" />
         </x-slot:actions>
