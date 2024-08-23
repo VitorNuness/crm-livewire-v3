022f14d36c4634a5b57ebbab0986141176207e66
VitorNuness <vitor1nuness@gmail.com>
2024-08-23 14:38:08 -0300

    crm-4: refactor how call layout in Login, Register and Password\Recovery

--- app/Livewire/Auth/Login.php
+++ app/Livewire/Auth/Login.php
@@ -5,6 +5,7 @@ namespace App\Livewire\Auth;
 use Illuminate\Contracts\View\View;
 use Illuminate\Support\Facades\{Auth, RateLimiter};
 use Illuminate\Support\Str;
+use Livewire\Attributes\Layout;
 use Livewire\Component;
 use Mary\Traits\Toast;
 
@@ -16,10 +17,10 @@ class Login extends Component
 
     public ?string $password = null;
 
+    #[Layout('components.layouts.guest')]
     public function render(): View
     {
-        return view('livewire.auth.login')
-            ->layout('components.layouts.guest');
+        return view('livewire.auth.login');
     }
 
     public function tryToLogin(): void
