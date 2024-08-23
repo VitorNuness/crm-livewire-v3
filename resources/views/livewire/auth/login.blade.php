fe63dc92ebecde98b118b2c3a4654746c02601fc
VitorNuness <vitor1nuness@gmail.com>
2024-08-23 14:37:10 -0300

    crm-4: refactor password recovery route name

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -9,6 +9,6 @@
             <x-button label="I wanto to create an account" :link="route('auth.register')" class="btn-ghost" />
             <x-button label="Login" class="btn-primary" type="submit" spinner="tryToLogin" />
         </x-slot:actions>
-        <x-button label="Forgot your password?" :link="route('auth.password.recovery')" class="btn-ghost" />
+        <x-button label="Forgot your password?" :link="route('password.recovery')" class="btn-ghost" />
     </x-form>
 </x-card>
--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\{Auth, Route};
 Route::get('/auth/login', Login::class)->name('login');
 Route::get('/auth/register', Register::class)->name('auth.register');
 Route::get('/auth/logout', fn () => Auth::logout());
-Route::get('/auth/password/recovery', Password\Recovery::class)->name('auth.password.recovery');
+Route::get('/auth/password/recovery', Password\Recovery::class)->name('password.recovery');
 Route::get('/password/reset', fn () => "oi")->name('password.reset');
 
 Route::middleware('auth')->group(function () {
