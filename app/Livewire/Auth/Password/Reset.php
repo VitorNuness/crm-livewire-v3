a747814d99ee2eb4fe03623b1782442b39498e59
VitorNuness <vitor1nuness@gmail.com>
2024-08-24 09:21:50 -0300

    crm-4: making sure the password has updated

--- app/Livewire/Auth/Password/Reset.php
+++ app/Livewire/Auth/Password/Reset.php
@@ -2,9 +2,11 @@
 
 namespace App\Livewire\Auth\Password;
 
+use App\Models\User;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\{Password};
+use Illuminate\Support\Str;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
 
@@ -14,24 +16,52 @@ class Reset extends Component
 
     public ?string $email = null;
 
+    public ?string $email_confirmation = null;
+
+    public ?string $password = null;
+
+    public ?string $password_confirmation = null;
+
     public function mount(Request $request): void
     {
-        $this->token = $request->get('token');
-        $this->email = $request->get('email');
+        $this->token = $request->get('token', $this->token);
+        $this->email = $request->get('email', $this->email);
 
-        if ($user = Password::getUser(['email' => $this->email])) {
-            if (Password::tokenExists($user, $this->token)) {
-                return;
-            }
+        if ($this->tokenNotValid()) {
+            session()->flash('status', trans('passwords.token'));
+            to_route('login');
         }
-
-        session()->flash('status', 'Invalid recovery token!');
-        to_route('login');
     }
 
     #[Layout('components.layouts.guest')]
     public function render(): View
     {
         return view('livewire.auth.password.reset');
+    }
+
+    public function updatePassword(): void
+    {
+        $status = Password::reset(
+            $this->only('email', 'password', 'password_confirmation', 'token'),
+            function (User $user, $password) {
+                $user->password = $password;
+                $user->setRememberToken(Str::random(60));
+                $user->save();
+            }
+        );
+
+        session()->flash('status', trans($status));
+        to_route('home');
+    }
+
+    private function tokenNotValid(): bool
+    {
+        $user = Password::getUser(['email' => $this->email]);
+
+        if (empty($user)) {
+            return true;
+        } else {
+            return !Password::tokenExists($user, $this->token);
+        }
     }
 }
--- tests/Feature/Auth/Password/ResetTest.php
+++ tests/Feature/Auth/Password/ResetTest.php
@@ -1,9 +1,13 @@
 <?php
 
+use App\Livewire\Auth\Password\Reset;
 use App\Models\User;
-use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Facades\{Hash, Password};
+use Livewire\Livewire;
 
-use function Pest\Laravel\get;
+use function Pest\Laravel\{get};
+
+use function PHPUnit\Framework\assertTrue;
 
 test('need to receive a valid token with a combination with the email', function () {
     $user  = User::factory()->create();
@@ -18,4 +22,26 @@ test('need to receive a valid token with a combination with the email', function
         'token' => 'token',
         'email' => 'email',
     ]))->assertRedirect(route('login'));
+});
+
+test('if is possible to reset the password with the given token', function () {
+    $user  = User::factory()->create();
+    $token = Password::createToken($user);
+
+    Livewire::test(Reset::class, [
+        'token' => $token,
+        'email' => $user->email,
+    ])
+        ->set('email_confirmation', $user->email)
+        ->set('password', 'new-password')
+        ->set('password_confirmation', 'new-password')
+        ->call('updatePassword')
+        ->assertHasNoErrors()
+        ->assertRedirect(route('home'));
+
+    $user->refresh();
+
+    assertTrue(
+        Hash::check('new-password', $user->password)
+    );
 });
