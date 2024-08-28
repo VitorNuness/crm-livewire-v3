8baf5f9ca60411baf3523696f964e3e7322ea784
VitorNuness <vitor1nuness@gmail.com>
2024-08-28 16:01:06 -0300

    crm-13: frontend: adding check box to filter deleted users and change action buttons in users list

--- resources/views/livewire/admin/user/index.blade.php
+++ resources/views/livewire/admin/user/index.blade.php
@@ -2,7 +2,7 @@
     <x-header title="Users" separator />
 
     <div class="flex my-4 justify-between">
-        <div class="flex flex-row space-x-4">
+        <div class="flex flex-row items-center space-x-4">
             <x-input label="Search by name or email" wire:model.live.debounce="search" clearable icon="o-magnifying-glass" class="!px-10"/>
 
             <x-choices
@@ -13,9 +13,14 @@
                 option-label="key"
                 clearable
             />
-        </div>
 
-        <x-button class="btn-primary" label="Create" responsive icon="o-plus" />
+        </div>
+        
+        <div class="flex flex-col space-y-4">
+            <x-button class="btn-primary" label="Create" responsive icon="o-plus" />
+
+            <x-checkbox label="Filter deleted users" wire:model.live="search_trash" right tight class="checkbox-primary" />
+        </div>
     </div>
 
     <x-table :headers="$this->headers" :rows="$this->users">
@@ -26,7 +31,14 @@
         @endscope
 
         @scope('actions', $user)
-            <x-button icon="o-pencil-square" spinner class="btn-sm" />
+            <div class="flex flex-row">
+                <x-button icon="o-pencil-square" spinner class="btn-sm" />
+                @if ($user->trashed())
+                    <x-button icon="o-arrow-left-start-on-rectangle" spinner class="btn-sm" />
+                @else
+                    <x-button icon="o-trash" spinner class="btn-sm" />
+                @endif
+            </div>
         @endscope
     </x-table>
 </div>
