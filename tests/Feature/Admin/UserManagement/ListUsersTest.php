d2786ee8488750c0ddf38cf9bc474e19c60b8de2
VitorNuness <vitor1nuness@gmail.com>
2024-08-28 11:09:28 -0300

    crm-13: making sure the route admin/users only access by admin users

--- tests/Feature/Admin/UserManagement/ListUsersTest.php
+++ tests/Feature/Admin/UserManagement/ListUsersTest.php
@@ -2,12 +2,22 @@
 
 use App\Models\User;
 
-use function Pest\Laravel\actingAs;
+use function Pest\Laravel\{actingAs};
 
 it('should be able to access the route admin/users', function () {
     $user = User::factory()->admin()->create();
 
     actingAs($user)
+        ->get(route('admin.users'))
+        ->assertOk();
+});
+
+test('make sure the route only access by admin users', function () {
+    actingAs(User::factory()->create())
+        ->getJson(route('admin.users'))
+        ->assertForbidden();
+
+    actingAs(User::factory()->admin()->create())
         ->get(route('admin.users'))
         ->assertOk();
 });
